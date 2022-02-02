c85c3b40ee37b590e5efcafd248fb148df249ddc
Kim Neunert <k9ert@gmx.de>
2022-02-02 12:21:55 +0100

    moar updates

---
+++ Readme.md
@@ -0,0 +1,28 @@
+This is a minimal extension which should get you started if you want to contribute to specter via an 
+extension.
+
+# Get it to run
+
+In oder to use this:
+```
+git clone git@github.com:cryptoadvance/specterext-dummy.git
+cd specterext-dummy
+pip3 install virtualenv
+virtualenv --python=python3 .env
+source .env/bin/activate
+pip3 install -r requirements.txt
+python3 -m cryptoadvance.specter server --config DevelopmentConfig --debug
+# point your browser to http://localhost:25441
+# "choose Services" --> dummy
+```
+
+![](./docs/images/dummy_service.gif)
+
+
+# Get Help
+
+If you want help or can't implement what you have in your mind or just want to say hello, join our Telegram-Chat.
+
+https://t.me/+yS4VO2JqBHkzYTI6
+
+If you want to 
--- dummy/controller.py
+++ dummy/controller.py
@@ -1,14 +1,13 @@
 import logging
-from flask import Flask, Response, redirect, render_template, request, url_for, flash
-from flask_login import login_required
+from flask import redirect, render_template, request, url_for, flash
+from flask import current_app as app
+from flask_login import login_required, current_user
 
 from cryptoadvance.specter.services.controller import user_secret_decrypted_required
+from cryptoadvance.specter.user import User
+from cryptoadvance.specter.wallet import Wallet
 from .service import DummyService
 
-
-"""
-    Empty placeholder just so the dummyservice/static folder can be wired up to retrieve its img
-"""
 
 logger = logging.getLogger(__name__)
 
@@ -22,3 +21,35 @@ def index():
     return render_template(
         "dummy/index.jinja",
     )
+
+@dummy_endpoint.route("/settings", methods=["GET"])
+@login_required
+@user_secret_decrypted_required
+def settings_get():
+    associated_wallet: Wallet = DummyService.get_associated_wallet()
+
+    # Get the user's Wallet objs, sorted by Wallet.name
+    wallet_names = sorted(current_user.wallet_manager.wallets.keys())
+    wallets = [current_user.wallet_manager.wallets[name] for name in wallet_names]
+
+    return render_template(
+        "dummy/settings.jinja",
+        associated_wallet=associated_wallet,
+        wallets=wallets,
+        cookies=request.cookies,
+    )
+
+@dummy_endpoint.route("/settings", methods=["POST"])
+@login_required
+@user_secret_decrypted_required
+def settings_post():
+    show_menu = request.form["show_menu"]
+    user = app.specter.user_manager.get_user()
+    if show_menu == "yes":
+        user.add_service(DummyService.id)
+    else:
+        user.remove_service(DummyService.id)
+    used_wallet_alias = request.form.get("used_wallet")
+    if used_wallet_alias != None:
+        wallet = current_user.wallet_manager.get_by_alias(used_wallet_alias)
+    return redirect(url_for(f"{DummyService.get_blueprint_name()}.settings_get"))
